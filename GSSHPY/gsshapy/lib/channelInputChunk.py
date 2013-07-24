85ed6a43898303af0e53b69182641242a51164f3
Nathan Swain <nathan.swain86@gmail.com>
2013-07-23 21:56:14 -0600

    Nightly commit. More work on channel input file read and write.

--- GSSHPY/gsshapy/lib/channelInputChunk.py
+++ GSSHPY/gsshapy/lib/channelInputChunk.py
@@ -36,18 +36,50 @@ def linkChunk(key, chunk):
         result = xSectionLink(chunk)
             
     elif linkType == 'STRUCTURE':
-        result = structureLink(key, chunk)
+        result = structureLink(chunk)
         
     elif linkType in ['RESERVOIR', 'LAKE']:
         result = reservoirLink(key, chunk)
     
     return result
 
-def structureLink(key, chunk):
+def structureLink(lines):
     '''
     Parse STRUCTURE LINK Method
     '''
-    return key, chunk
+    KEYWORDS = ['LINK',
+                'STRUCTURE',
+                'NUMSTRUCTS',
+                'STRUCTTYPE']
+    
+    WEIRS = ['WEIR', 'SAG_WEIR']
+    
+    CULVERTS = ['ROUND_CULVERT', 'RECT_CULVERT']
+    
+    CURVES = ['RATING_CURVE', 'SCHEDULED_RELEASE', 'RULE_CURVE']
+    
+    result = {'header': [],
+              'structures':[]}
+    
+    
+    chunks = pt.chunk(KEYWORDS, lines)
+    
+    # Parse chunks associated with each key    
+    for key, chunkList in chunks.iteritems():
+        # Parse each chunk in the chunk list
+        for chunk in chunkList:
+            if key == 'STRUCTTYPE':
+                structType = chunk[0].strip().split()[1]
+                if structType in WEIRS:
+                    result['structures'].append(weirChunk(chunk))
+                elif structType in CULVERTS:
+                    result['structures'].append(culvertChunk(chunk))
+                elif structType in CURVES:
+                    pass
+            else:
+                result['header'].append(cardChunk(key, chunk))
+               
+    return result
     
 def xSectionLink(lines):
     '''
@@ -79,11 +111,17 @@ def xSectionLink(lines):
     for key, chunkList in chunks.iteritems():
         # Parse each chunk in the chunk list
         for chunk in chunkList:
+            # Cases
             if key == 'NODE':
+                # Extract node x and y
                 result['nodes'].append(nodeChunk(chunk))
+                
             elif key == 'XSEC':
-                result['xSection'] = xSecChunk(chunk)
+                # Extract cross section information
+                result['xSection'] = xSectionChunk(chunk)
+                
             else:
+                # Extract all other variables into header
                 result['header'].append(cardChunk(key, chunk))
     return result
 
@@ -121,7 +159,7 @@ def nodeChunk(lines):
                 
     return result
     
-def xSecChunk(lines):
+def xSectionChunk(lines):
     '''
     Parse XSEC Method
     '''
@@ -150,14 +188,85 @@ def xSecChunk(lines):
               'm_river': None,
               'k_river': None,
               'breakpoints': []}
+    
     chunks = pt.chunk(KEYWORDS, lines)
     
     # Parse chunks associated with each key    
     for key, chunkList in chunks.iteritems():
         # Parse each chunk in the chunk list
         for chunk in chunkList:
-            print chunk
-    print result
+            # Strip and split the line (only one item in each list)
+            schunk = chunk[0].strip().split()
+            
+            # Cases
+            if key == 'X1':
+                # Extract breakpoint XY pairs
+                x = schunk[1]
+                y = schunk[2]
+                result['breakpoints'].append({'x': x, 'y': y})
+                
+            if key in ['SUBSURFACE', 'ERODE']:
+                # Set booleans
+                result[key.lower()] = True
+                
+            else:
+                # Extract value
+                result[key.lower()] = schunk[1]
+    return result
+
+def weirChunk(lines):
+    '''
+    Parse Weir Structures Method
+    '''
+    KEYWORDS = ['STRUCTTYPE',
+                'CREST_LENGTH',
+                'CREST_LOW_ELEV',
+                'DISCHARGE_COEFF_FORWARD',
+                'DISCHARGE_COEFF_REVERSE',
+                'CREST_LOW_LOC',
+                'STEEP_SLOPE',
+                'SHALLOW_SLOPE']
+    
+    result = {}
+    chunks = pt.chunk(KEYWORDS, lines)
+    
+    # Parse chunks associated with each key    
+    for key, chunkList in chunks.iteritems():
+        # Parse each chunk in the chunk list
+        for chunk in chunkList:
+            # Strip and split the line (only one item in each list)
+            schunk = chunk[0].strip().split()
+            print schunk
+    
+    return result
+    
+def culvertChunk(lines):
+    '''
+    Parse Culvert Structures Method
+    '''
+    KEYWORDS = ['STRUCTTYPE',
+                'UPINVERT',
+                'DOWNINVERT',
+                'INLET_DISCH_COEFF',
+                'REV_FLOW_DISCH_COEFF',
+                'SLOPE',
+                'LENGTH',
+                'ROUGH_COEFF',
+                'DIAMETER',
+                'WIDTH',
+                'HEIGHT']
+    
+    result = {}
+    chunks = pt.chunk(KEYWORDS, lines)
+    
+    # Parse chunks associated with each key    
+    for key, chunkList in chunks.iteritems():
+        # Parse each chunk in the chunk list
+        for chunk in chunkList:
+            # Strip and split the line (only one item in each list)
+            schunk = chunk[0].strip().split()
+            print schunk
+    
     return result
     
 
