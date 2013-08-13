68e55c1e2a6dc9defad052ca28ff6c083f5779fb
Nathan Swain <nathan.swain86@gmail.com>
2013-08-13 15:46:57 -0600

    Commits

--- GSSHAPY/setup.py
+++ GSSHAPY/setup.py
@@ -11,7 +11,7 @@ requires = [
     ]
 
 setup(name='gsshapy',
-      version='0.1.0',
+      version='1.0.0',
       description='An SQLAlchemy ORM for GSSHA model files.',
       long_description=README,
       author='Nathan Swain',
