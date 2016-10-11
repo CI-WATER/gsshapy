2e06386fe8ab2cefab02a37fc009091fcca49284
snowman2 <alan.d.snow@usace.army.mil>
2016-10-11 12:08:48 -0500

    updated retrieving centroid for GSSHA model & added doc strings

--- gridtogssha/gssha_framework.py
+++ gridtogssha/gssha_framework.py
@@ -7,12 +7,14 @@
 ##  BSD 3-Clause
 
 from datetime import datetime
-from gridtogssha import LSMtoGSSHA
-from gsshapy.lib import db_tools as dbt
-from gsshapy.orm import ProjectCard, ProjectFile
+from pyproj import Proj, transform
 import os
 from RAPIDpy import RAPIDDataset
 from subprocess import Popen, PIPE
+
+from gridtogssha import LSMtoGSSHA
+from gsshapy.lib import db_tools as dbt
+from gsshapy.orm import ProjectCard, ProjectFile
 
 class GSSHAFramework(object):
     def __init__(self, gssha_executable, gssha_directory, project_filename):
@@ -81,6 +83,74 @@ class GSSHAFramework(object):
         
         """
         Updates card & runs for RAPID to GSSHA & LSM to GSSHA
+        
+        Args:
+            path_to_rapid_qout(str): Path to the RAPID Qout file.
+            connection_list(list): List connecting GSSHA rivers to RAPID river network.
+            lsm_folder(str): Path to folder with land surface model data. See: *lsm_input_folder_path* variable at :func:`~gridtogssha.grid_to_gssha.GRIDtoGSSHA`.
+            lsm_data_var_map_array(str): Array with connections for LSM output and GSSHA input. See: :func:`~gridtogssha.grid_to_gssha.GRIDtoGSSHA.`
+            lsm_precip_data_var(list or str): String of name for precipitation variable name or list of precip variable names.  See: :func:`~gridtogssha.grid_to_gssha.GRIDtoGSSHA.lsm_precip_to_gssha_precip_gage`.
+            lsm_precip_type(str): Type of precipitation. See: :func:`~gridtogssha.grid_to_gssha.GRIDtoGSSHA.lsm_precip_to_gssha_precip_gage`.
+            lsm_lat_var(str): Name of the latitude variable in the LSM netCDF files. See: :func:`~gridtogssha.LSMtoGSSHA`.
+            lsm_lon_var(str): Name of the longitude variable in the LSM netCDF files. See: :func:`~gridtogssha.LSMtoGSSHA`.
+            lsm_file_date_naming_convention(str): Array with connections for LSM output and GSSHA input. See: :func:`~gridtogssha.LSMtoGSSHA`.
+            lsm_time_var(str): Name of the time variable in the LSM netCDF files. See: :func:`~gridtogssha.LSMtoGSSHA`.
+            lsm_search_card(str): Glob search pattern for LSM files. See: :func:`~gridtogssha.grid_to_gssha.GRIDtoGSSHA`.
+            output_netcdf(bool): If you want the HMET data output as a NetCDF4 file for input to GSSHA. Default is False.
+            
+        Example::
+            
+                gssha_executable = 'C:/Program Files/WMS 10.1 64-bit/gssha/gssha.exe'
+                gssha_directory = "C:/Users/{username}/Documents/GSSHA"
+                project_filename = "gssha_project.prj"
+                
+                #RAPID INPUTS
+                path_to_rapid_qout = 'C:/Users/{username}/Documents/GSSHA/Qout_rapid_watershed.nc'
+                #list to connect the RAPID rivers to GSSHA rivers
+                connection_list = [
+                                   {
+                                     'link_id': 599,
+                                     'node_id': 1,
+                                     'baseflow': 0.0,
+                                     'rapid_rivid': 80968,
+                                   },
+                                 ]
+                
+                #WRF INPUTS
+                lsm_folder = '"C:/Users/{username}/Documents/GSSHA/wrf-sample-data-v1.0'
+                lsm_lat_var = 'XLAT'
+                lsm_lon_var = 'XLONG'
+                search_card = '*.nc'
+                precip_data_var = ['RAINC', 'RAINNC']
+                precip_type = 'ACCUM'
+                lsm_file_date_naming_convention='gssha_d02_%Y_%m_%d_%H_%M_%S.nc'
+                
+                data_var_map_array = [
+                                      ['precipitation_acc', ['RAINC', 'RAINNC']], 
+                                      ['pressure', 'PSFC'], 
+                                      ['relative_humidity', ['Q2', 'PSFC', 'T2']], 
+                                      ['wind_speed', ['U10', 'V10']], 
+                                      ['direct_radiation', ['SWDOWN', 'DIFFUSE_FRAC']],
+                                      ['diffusive_radiation', ['SWDOWN', 'DIFFUSE_FRAC']],
+                                      ['temperature', 'T2'],
+                                      ['cloud_cover' , 'CLDFRA'],
+                                     ]
+                
+                #INITIALIZE CLASS AND RUN
+                gr = GSSHAFramework(gssha_executable,
+                                    gssha_directory, 
+                                    project_filename)
+                
+                gr.run(path_to_rapid_qout, 
+                       connection_list,
+                       lsm_folder,
+                       data_var_map_array,
+                       precip_data_var,
+                       precip_type,
+                       lsm_lat_var,
+                       lsm_lon_var,
+                       lsm_file_date_naming_convention,
+                       )
         """
         self._update_card("PROJECT_PATH", self.gssha_directory)
         
@@ -111,11 +181,6 @@ class GSSHAFramework(object):
         #----------------------------------------------------------------------
         #LSM to GSSHA
         #----------------------------------------------------------------------
-        
-        ###### ----------
-        ### MAIN FUNNCTIONS
-        ###### ----------
-    
         l2g = LSMtoGSSHA(gssha_project_folder=self.gssha_directory,
                          gssha_grid_file_name='{0}.ele'.format(self.project_name),
                          lsm_input_folder_path=lsm_folder,
@@ -145,12 +210,19 @@ class GSSHAFramework(object):
         # update cards
         self._update_card('LONG_TERM', '')
         self._update_card('PRECIP_FILE', out_gage_file)
-        #TODO: UPDATE CARDS
-        #self._update_card('GMT', '')
-        #self._update_card('LATITUDE', '')
-        #self._update_card('LONGITUDE', '')
+        #assume UTC time zone
+        self._update_card('GMT', str(0))
+        #update centroid
+        center_lon, center_lat = transform(l2g.gssha_proj4,
+                                           Proj(init='epsg:4326'),
+                                           [(l2g.east_bound+l2g.west_bound)/2.0],
+                                           [(l2g.north_bound+l2g.south_bound)/2.0], 
+                                           )
+        
+        self._update_card('LATITUDE', str(center_lat[0]))
+        self._update_card('LONGITUDE', str(center_lon[0]))
 
-        #WRITE OUT NEW FILES
+        #WRITE OUT UPDATED GSSHA PROJECT FILE
         self.project_manager.write(session=self.db_session, 
                                    directory=self.gssha_directory, 
                                    name=self.project_name)
