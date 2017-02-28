b90033e77b9dda9c1596875592b0102d6bd50b7e
Alan D. Snow <alan.d.snow@usace.army.mil>
2017-02-28 16:39:06 -0600

    added test generating project and adding roughness from land cover

--- tests/project_generation_tests.py
+++ tests/project_generation_tests.py
@@ -13,7 +13,8 @@ from shutil import copy
 
 from .template import TestGridTemplate
 
-from gsshapy.orm import ProjectFile, WatershedMaskFile, ElevationGridFile
+from gsshapy.orm import (ProjectFile, WatershedMaskFile, ElevationGridFile,
+                         MapTableFile)
 from gsshapy.lib import db_tools as dbt
 
 from os import path, chdir
@@ -31,6 +32,7 @@ class TestProjectGenerate(TestGridTemplate):
 
         self.elevation_path = path.join(self.writeDirectory,
                                         'gmted_elevation.tif')
+        self.land_use_grid = path.join(self.writeDirectory, 'LC_hd_global_2012.tif')
 
         # copy shapefile
         shapefile_basename = path.join(self.readDirectory,
@@ -49,6 +51,13 @@ class TestProjectGenerate(TestGridTemplate):
             copy(path.join(self.readDirectory, 'phillipines',
                            'gmted_elevation.tif'),
                  self.elevation_path)
+        except OSError:
+            pass
+
+        # copy land cover data
+        try:
+            copy(path.join(self.readDirectory, 'land_cover', 'LC_hd_global_2012.tif'),
+                 self.land_use_grid)
         except OSError:
             pass
 
@@ -146,6 +155,98 @@ class TestProjectGenerate(TestGridTemplate):
         generated_proj_file = path.join(self.gssha_project_directory, proj_file_name)
         compare_proj_file = path.join(self.compare_path, proj_file_name)
         self._compare_files(generated_proj_file, compare_proj_file)
+
+    def test_generate_basic_project_land_cover(self):
+        '''
+        Tests generating a basic GSSHA project with land cover
+        '''
+
+        project_name = "grid_standard_basic_land_cover"
+        # Instantiate GSSHAPY object for reading to database
+        project_manager = ProjectFile(name=project_name, map_type=1)
+        self.db_session.add(project_manager)
+        self.db_session.commit()
+
+        # ADD MASK
+        mask_name = '{0}.msk'.format(project_name)
+        msk_file = WatershedMaskFile(project_file=project_manager,
+                                     session=self.db_session)
+
+        msk_file.generateFromWatershedShapefile(self.shapefile_path,
+                                                mask_name,
+                                                x_cell_size=1000,
+                                                y_cell_size=1000,
+                                                )
+
+        # ADD ELEVATION FILE
+        ele_file = ElevationGridFile(project_file=project_manager,
+                                     session=self.db_session)
+        ele_file.generateFromRaster(self.elevation_path)
+
+        # ADD OUTLET POINT
+        grid = project_manager.getGrid()
+        lon, lat = grid.pixel2lonlat(0,6)
+
+        project_manager.setOutlet(latitude=lat, longitude=lon,
+                                  outslope=0.002)
+
+        # ADD ROUGHNESS FROM LAND COVER
+        # see http://www.gsshawiki.com/Project_File:Overland_Flow_%E2%80%93_Required
+        mapTableFile = MapTableFile(project_file=project_manager)
+        mapTableFile.addRoughnessMapFromLandUse("roughness",
+                                                self.db_session,
+                                                self.land_use_grid,
+                                                land_use_grid_id='glcf',
+                                                )
+
+        # ADD ADDITIONAL REQUIRED FILES
+        # see http://www.gsshawiki.com/Project_File:Required_Inputs
+        project_manager.setCard('TOT_TIME', '180')
+        project_manager.setCard('TIMESTEP', '10')
+        project_manager.setCard('HYD_FREQ', '15')
+        # see http://www.gsshawiki.com/Project_File:Output_Files_%E2%80%93_Required
+        project_manager.setCard('SUMMARY', '{0}.sum'.format(project_name), add_quotes=True)
+        project_manager.setCard('OUTLET_HYDRO', '{0}.otl'.format(project_name), add_quotes=True)
+        # see http://www.gsshawiki.com/Project_File:Rainfall_Input_and_Options_%E2%80%93_Required
+        project_manager.setCard('PRECIP_UNIF', '')
+        project_manager.setCard('RAIN_INTENSITY', '2.4')
+        project_manager.setCard('RAIN_DURATION', '30')
+        project_manager.setCard('START_DATE', '2017 02 28')
+        project_manager.setCard('START_TIME', '14 33')
+
+        # write data
+        project_manager.writeInput(session=self.db_session,
+                                   directory=self.gssha_project_directory,
+                                   name=project_name)
+        # compare msk
+        self._compare_masks(mask_name)
+        # compare ele
+        ele_grid_name = '{0}.ele'.format(project_name)
+        new_mask_grid = path.join(self.gssha_project_directory, ele_grid_name)
+        compare_msk_file = path.join(self.compare_path, ele_grid_name)
+        self._compare_files(compare_msk_file, new_mask_grid, raster=True)
+        # compare cmt
+        cmt_file_name = '{0}.cmt'.format(project_name)
+        new_cmt_file = path.join(self.gssha_project_directory, cmt_file_name)
+        compare_cmt_file = path.join(self.compare_path, cmt_file_name)
+        self._compare_files(new_cmt_file, compare_cmt_file)
+        # compare idx
+        new_idx_file = path.join(self.gssha_project_directory, 'roughness.idx')
+        original_idx_file = path.join(self.compare_path, 'roughness.idx')
+        self._compare_files(original_idx_file, new_idx_file, raster=True)
+        # compare project files
+        prj_file_name = '{0}.prj'.format(project_name)
+        generated_prj_file = path.join(self.gssha_project_directory, prj_file_name)
+        compare_prj_file = path.join(self.compare_path, prj_file_name)
+        self._compare_files(generated_prj_file, compare_prj_file)
+        # check to see if projection file generated
+        proj_file_name = '{0}_prj.pro'.format(project_name)
+        generated_proj_file = path.join(self.gssha_project_directory, proj_file_name)
+        compare_proj_file = path.join(self.compare_path, proj_file_name)
+        self._compare_files(generated_proj_file, compare_proj_file)
+
+    def tearDown(self):
+        return
 
 if __name__ == '__main__':
     unittest.main()
