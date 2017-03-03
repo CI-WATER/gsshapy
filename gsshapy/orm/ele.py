56eca2d2ec6cac9d87b2c01be248aa4cfbf0203a
Alan D. Snow <alan.d.snow@usace.army.mil>
2017-03-03 13:31:09 -0600

    added slope calculation to elevation process

--- gsshapy/orm/ele.py
+++ gsshapy/orm/ele.py
@@ -9,6 +9,7 @@
 
 __all__ = ['ElevationGridFile']
 
+import numpy as np
 import os
 from osgeo import gdalconst
 from ..grid.grid_tools import resample_grid
@@ -38,7 +39,8 @@ class ElevationGridFile(RasterMapFile):
 
     def generateFromRaster(self, elevation_raster,
                            out_elevation_grid=None,
-                           resample_method=gdalconst.GRA_Average):
+                           resample_method=gdalconst.GRA_Average,
+                           calculate_outlet_slope=True):
         '''
         Generates an elevation grid for the GSSHA simulation
         from an elevation raster
@@ -81,7 +83,7 @@ class ElevationGridFile(RasterMapFile):
             raise ValueError("Must be connected to project file ...")
 
         # must match elevation mask grid
-        match_grid = self.projectFile.getGrid()
+        mask_grid = self.projectFile.getGrid()
         if out_elevation_grid is None:
             project_path = ''
             project_path_card = self.projectFile.getCard('PROJECT_PATH')
@@ -92,13 +94,44 @@ class ElevationGridFile(RasterMapFile):
                                                                self.fileExtension),
                                               )
 
-        gd = resample_grid(elevation_raster,
-                           match_grid,
-                           resample_method=resample_method,
-                           as_gdal_grid=True)
-        gd.to_grass_ascii(out_elevation_grid, print_nodata=False)
+        elevation_grid = resample_grid(elevation_raster,
+                                       mask_grid,
+                                       resample_method=resample_method,
+                                       as_gdal_grid=True)
+        elevation_grid.to_grass_ascii(out_elevation_grid, print_nodata=False)
 
         self.filename = out_elevation_grid
         self.projectFile.setCard("ELEVATION", out_elevation_grid, add_quotes=True)
         # read raster into object
         self._load_raster_text(out_elevation_grid)
+
+        # attempt to determine the slope at the OUTLET
+        if calculate_outlet_slope:
+            outrow = int(self.projectFile.getCard("OUTROW").value)-1
+            outcol = int(self.projectFile.getCard("OUTCOL").value)-1
+            cell_size = float(self.projectFile.getCard("GRIDSIZE").value)
+
+            min_row = max(0, outrow-1)
+            max_row = min(mask_grid.x_size(), outrow+2)
+            min_col = max(0, outcol-1)
+            max_col = min(mask_grid.y_size(), outcol+2)
+
+            mask_array = mask_grid.np_array()
+            mask_array[outrow, outcol] = 0
+            mask_array = mask_array[min_row:max_row, min_col:max_col]
+            mask_array = (mask_array==0)
+
+            elevation_array = elevation_grid.np_array()
+            original_elevation = elevation_array[outrow, outcol]
+            elevation_array = elevation_array[min_row:max_row, min_col:max_col]
+
+            slope_calc_array = (elevation_array-original_elevation)/cell_size
+            #NOTE: Ignoring distance to cells at angles. Assuming to small to matter
+            mask_array[slope_calc_array<=0] = True
+
+            slope_mask_array = np.ma.array(slope_calc_array, mask=mask_array)
+            outslope = slope_mask_array.mean()
+            if outslope is np.ma.masked or outslope < 0.001:
+                outslope = 0.001
+
+            self.projectFile.setCard("OUTSLOPE", str(outslope))
